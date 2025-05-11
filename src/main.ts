76fb20d65b38c96dd867ab693b7d057086a20f0a
PatrizioFiori <patrizio.fiori1995@gmail.com>
2025-05-11 18:06:46 +0200

    Refactor main.ts to remove unused imports and add basic variable declarations; update tsconfig.json to modify library settings and disable unused locals and parameters checks.

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,15 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+let helloWorld: string = "hello world"
+let number: number = 123
+let arrayNumber: number[] = [1, 2, 3]
+let arrayString: string[] = ["A", "B", "C"]
+let obj: { nome: string, eta: number, } = { nome: "Marco Rossi", eta: 25 }
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
+console.log(helloWorld);
+console.log(number);
+console.log(arrayNumber)
+console.log(arrayString);
+console.log(obj);
+
+
